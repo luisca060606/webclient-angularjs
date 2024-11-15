b8ef7749e16285ba480d0f4edff87cc5255e0070
Luis CA <luis.ca0804@gmail.com>
2024-11-14 20:33:36 -0400

    [WC-001] add config vite for build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   build: {
-    lib: {
-    }
+    outDir: 'dist',
+    assetsDir: '',
+    sourcemap: false,
+    minify: true,
   },
   server: {
     port: 9000,
